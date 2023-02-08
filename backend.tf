d60552568c1039c2f52c9aeeb0ef234161d660d8
Nanap4212 <nanap5625@gmail.com>
2023-02-08 09:29:27 +0530

    dynamodb changes

--- backend.tf
+++ backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "terraform-jenkins-project-1"
     key            = "terraform.tfstate"
-    profile        = "terraform"
     region         = "ap-northeast-1"
     dynamodb_table = "my-dynamodb-table-4212"
     encrypt = true
