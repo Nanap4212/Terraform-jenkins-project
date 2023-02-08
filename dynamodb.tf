789622d56fa59afb83de0bd166ad87c6841ffba5
Nanap4212 <nanap5625@gmail.com>
2023-02-08 09:18:49 +0530

    dynamodb changes

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,6 +1,9 @@
 resource "aws_dynamodb_table" "Dynamodb" {
   name     = var.environment
   hash_key = "LockID"
+  billing_mode = "PROVISIONED"
+  read_capacity= "30"
+  write_capacity= "30"
   attribute {
     name = "LockID"
     type = "S"
